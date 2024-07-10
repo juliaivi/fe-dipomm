97d0ea210c2db385d61a844a741201a057a92f2d
juliaivi <juliashinobi@mail.ru>
2024-07-10 15:20:22 +0300

    v1

--- src/components/Passengers/TripDetails/conversionData.js
+++
@@ -1,6 +0,0 @@
-export const conversionData = (data) => {
-    const newData = new Date(data);
-    const date=newData.getDate() < 10 ? '0' + newData.getDate() : newData.getDate();
-    const month = newData.getMonth() < 10 ? '0' + newData.getMonth() : newData.getMonth();
-    return (date+'.'+month+'.'+newData.getFullYear());
-}
